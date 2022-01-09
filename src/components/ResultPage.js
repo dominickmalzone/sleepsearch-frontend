e427e0dd832a86d8be12507a6ae2700b9c616c1b
malzone <dominickmalzone@aol.com>
2022-01-08 22:12:16 -0700

    search working

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -4,17 +4,21 @@ import { useSearchParams } from "react-router-dom";
 function ResultPage() {
   const [result, setResult] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
+  // const [loading, setLoading] = useState(false);
 
-  const SERVER_URL = "http://localhost:5000";
+  const SERVER_URL = "https://7bf9-125-164-13-145.ngrok.io"; //"http://localhost:5000";
 
   const runSearchQuery = (searchPhrase) => {
     console.log("runSearchQuery");
     try {
+      //set loading
       fetch(`${SERVER_URL}/search?data=${searchPhrase}`)
         .then((response) => response.json())
         .then((data) => {
-          console.log(`Results here ${data}`);
-          setResult(data);
+          let responseText = data.result;
+          console.log("Response text > ", responseText);
+          // console.log(`Results here ${JSON.stringify(data)}`);
+          setResult(responseText);
         });
     } catch (e) {
       alert("Something went wrong: ", e);
@@ -27,9 +31,9 @@ function ResultPage() {
     console.log("Calling search api with phrase: ", searchTerm);
     runSearchQuery(searchTerm);
 
-    setResult(
-      `Here is where the results will be for your search: ${searchTerm}`
-    );
+    // setResult(
+    //   `Here is where the results will be for your search: ${searchTerm}`
+    // );
   }, []);
 
   const ResultsPage = () => {};
@@ -39,12 +43,16 @@ function ResultPage() {
       <h1 className="home-title title-result">Sleep Search</h1>
       <h3 className="sub-result">Ask any sleep questions here</h3>
       <div className="result-wrapper">
-        <div className="result-container">
-          <p className="result-paragraph">
-            result:
-            {result}
-          </p>
-        </div>
+        {!result && (
+          <div className="loading-container">
+            <p>Loading...</p>
+          </div>
+        )}
+        {result && (
+          <div className="result-container">
+            <p className="result-paragraph">{result}</p>
+          </div>
+        )}
       </div>
     </div>
   );

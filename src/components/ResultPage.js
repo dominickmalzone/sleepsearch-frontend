e253c769d4a9b18dee56dbd11fba5d36df502d23
malzone <dominickmalzone@aol.com>
2022-01-08 21:20:18 -0700

    making search functional

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,34 +1,58 @@
-import axios from "axios"
-import React, { useState, useEffect } from "react"
+// import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function ResultPage() {
-    const [result, setResult] = useState("")
+  const [result, setResult] = useState("");
 
-    const ResultsPage = () => {
-        useEffect(() => {
-        axios.get('http://localhost:5000/search').then(res => res.json()).then(
-            result => {
-                setResult(result)
-                console.log(result)
-            }
-        )
-        })
+  const SERVER_URL = "http://localhost:5000";
+
+  const runSearchQuery = () => {
+    console.log("runSearchQuery");
+    try {
+      fetch(`${SERVER_URL}/search`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(`Results here ${data}`);
+          setResult(data);
+        });
+    } catch (e) {
+      alert("Something went wrong: ", e);
     }
+  };
 
+  useEffect(() => {
+    console.log("UseEffect");
+  }, []);
 
+  const ResultsPage = () => {
+    // useEffect(() => {
+    //   axios
+    //     .get(`${SERVER_URL}/search`)
+    //     .then((res) => {
+    //       res.json();
+    //       console.log("res.json");
+    //     })
+    //     .then((result) => {
+    //       setResult(result);
+    //       console.log("results are back", result);
+    //     });
+    // });
+  };
 
-
-    return(
-        <div className="body">
-            <h1 className="home-title title-result">Sleep Search</h1>
-            <h3 className="sub-result">Ask any sleep questions here</h3>
-            <div className="result-wrapper">
-            <div className="result-container">
-                <p className="result-paragraph"></p>
-            </div>
-            </div>
+  return (
+    <div className="body">
+      <h1 className="home-title title-result">Sleep Search</h1>
+      <h3 className="sub-result">Ask any sleep questions here</h3>
+      <div className="result-wrapper">
+        <div className="result-container">
+          <p className="result-paragraph">
+            result:
+            {result}
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default ResultPage
+export default ResultPage;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,73 +1,78 @@
-import { Form, Button } from "react-bootstrap"
-import axios from "axios"
-import React, {useState} from "react"
-import { Link } from "react-router-dom"
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function SearchBar() {
-    function handleSubmit(e){
-        e.preventDefault();
-        var formData = new FormData(e.target);
-        var data = formData.get("data");
-        console.log('Your input value is: ' + data);
-        window.location.href = "http://localhost:5000/search?data=" + data;
-    }
-    return(
-        <div className="body">
-            <div className="form-wrapper">
-            <Form className="search-form" onSubmit={handleSubmit}>
-                <Form.Group className="mb-3 search-bar" controlId="formBasicEmail">
-                        <Form.Control name="data" placeholder="How do I achieve this?"/>
-                </Form.Group>
-                <Button className="Link btn-answer" variant="success" type="submit"><Link to="/results" className="Link">Answer</Link></Button>
-            </Form>
-            </div>
-        </div>
-    )
+  const [searchQuery, setsearchQuery] = useState();
+
+  const handleChange = async (e) => {
+    setsearchQuery(e.target.value);
+  };
+
+  return (
+    <div className="body">
+      <div className="form-wrapper">
+        <Form className="search-form">
+          <Form.Group className="mb-3 search-bar" controlId="formBasicEmail">
+            <Form.Control
+              name="data"
+              placeholder="How do I know if I have a sleep disorder?"
+              onChange={(e) => handleChange(e)}
+            />
+          </Form.Group>
+          <Button className="Link btn-answer" variant="success" type="submit">
+            <Link to={`/results?q=${searchQuery}`} className="Link">
+              Answer
+            </Link>
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
 
-
 // COMMENTS
 
-    // function handleFormSubmit(e) {
-    //     e.preventDefault();
-    //     console.log(this.state);
-    //     axios({
-    //         method:'post',
-    //         url:"http://localhost:5000/search",
-    //         headers: {'content-type': 'application/json'},
-    //         data:this.state
-    //     })
-    //     .then(result => {
-    //         this.setState({results:result.data});
-    //         console.log(result.data);
-    //     })
-    //     .catch(error=> {
-    //         console.log(error);
-    //     })
-    // }
-        // }
-        // const searchEngine = () => {
-        // let userSearch = {
-        //     data: search
-        // }
-        // if (search !== "") {
-        //     axios
-        //       .post("http://localhost:5000/search", userSearch)
-              
-        //       .then(function (response) {
-        //         console.log(response);
-        //         //Perform action based on response
-        //       })
-        //       .catch(function (error) {
-        //         console.log(error);
-        //         //Perform action based on error
-        // })
-        // } else {
-        //     alert("The search query cannot be empty")
-        // }}
-    // const [search, setSearch] = useState("")
-    // let state = {
-    //     data: search
-    // }
+// function handleFormSubmit(e) {
+//     e.preventDefault();
+//     console.log(this.state);
+//     axios({
+//         method:'post',
+//         url:"http://localhost:5000/search",
+//         headers: {'content-type': 'application/json'},
+//         data:this.state
+//     })
+//     .then(result => {
+//         this.setState({results:result.data});
+//         console.log(result.data);
+//     })
+//     .catch(error=> {
+//         console.log(error);
+//     })
+// }
+// }
+// const searchEngine = () => {
+// let userSearch = {
+//     data: search
+// }
+// if (search !== "") {
+//     axios
+//       .post("http://localhost:5000/search", userSearch)
 
+//       .then(function (response) {
+//         console.log(response);
+//         //Perform action based on response
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//         //Perform action based on error
+// })
+// } else {
+//     alert("The search query cannot be empty")
+// }}
+// const [search, setSearch] = useState("")
+// let state = {
+//     data: search
+// }
